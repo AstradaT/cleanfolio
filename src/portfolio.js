fe4dff1b06df3dc2d98ef85ee61fc873a71f9a83
AstradaT <tomi_cuervocasla@hotmail.com>
2022-11-01 20:50:54 -0300

    Add projects

--- src/portfolio.js
+++ src/portfolio.js
@@ -10,7 +10,7 @@ const about = {
   role: 'Data Engineer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+  resume: 'https://docs.google.com/document/d/1qUwFZtSk4VUVrnHtah1OAebzErShhQG1aDqc6FgFxZY/edit?usp=sharing',
   social: {
     linkedin: 'https://www.linkedin.com/in/tom%C3%A1s-astrada-370b73171/',
     github: 'https://github.com/AstradaT',
@@ -21,28 +21,28 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Amazon Reviews',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Group Project. BI Dashboard, Recommendation System and Sentiment Analysis.',
+    stack: ['Azure', 'PowerBI', 'NLP'],
+    sourceCode: 'https://github.com/dhugueth/DTS03-06_Amazon_Reviews_PF',
+    livePreview: 'https://astradat-dts03-06-amazon-reviews-pf-streamlitmain-4idio3.streamlitapp.com/',
   },
   {
-    name: 'Project 2',
+    name: 'Cryptocurrency Dashboard',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Streamlit dashboard to display data of cryptocurrencies fetched from FTX API',
+    stack: ['Python', 'API'],
+    sourceCode: 'https://github.com/AstradaT/PI03-Analytics',
+    livePreview: 'https://astradat-pi03-analytics-0--dashboard-cplxbs.streamlitapp.com/',
   },
   {
-    name: 'Project 3',
+    name: 'F1 World Championship API',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'API built to query a data warehouse with data from F1 World Championship dataset',
+    stack: ['Python', 'API', 'SQL'],
+    sourceCode: 'https://github.com/AstradaT/PI01_DATA03',
+    livePreview: 'pi01data03-production.up.railway.app/',
   },
 ]
 
