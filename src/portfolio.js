cb920cd832c15bdcc9c4ebae22fb131f9002120c
AstradaT <tomi_cuervocasla@hotmail.com>
2022-11-01 17:58:19 -0300

    Change contents

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,19 +1,19 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'Tomi\'s Portfolio.',
+  title: 'My Portfolio.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Tomas Astrada',
+  role: 'Data Engineer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/tom%C3%A1s-astrada-370b73171/',
+    github: 'https://github.com/AstradaT',
   },
 }
 
@@ -49,23 +49,27 @@ const projects = [
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
+  'Python',
+  'SQL',
+  'Jupyter',
+  'Pandas',
+  'Azure',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'Spark',
+  'MinIO',
+  'Hadoop',
+  'Docker',
+  'Airflow',
+  'Kafka',
+  'NiFi',
+  'Linux',
+  'Machine Learning',
+  'PowerBI',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'tomasastrada907@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
